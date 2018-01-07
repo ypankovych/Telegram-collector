40219b18a39424f8075b9821ce98ce9d25fc6c88
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-01-07 15:40:22 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -18,60 +18,60 @@ limit = 100
 
 
 def get_chat_info(username, client):
-	try:
-		chat = client(ResolveUsernameRequest(username))
-	except UsernameNotOccupiedError:
-		print('Chat/channel not found!')
-		sys.exit()
-	result = {
-		'chat_id': chat.peer.channel_id,
-		'access_hash': chat.chats[0].access_hash
-	}
-	return result
+    try:
+        chat = client(ResolveUsernameRequest(username))
+    except UsernameNotOccupiedError:
+        print('Chat/channel not found!')
+        sys.exit()
+    result = {
+        'chat_id': chat.peer.channel_id,
+        'access_hash': chat.chats[0].access_hash
+    }
+    return result
 
 
 def dump_users(chat, client):
-	counter = 0
-	offset = 0
-	chat_object = InputChannel(chat['chat_id'], chat['access_hash'])
-	all_participants = []
-	print('Process...')
-	while True:
-		participants = client.invoke(GetParticipantsRequest(
+    counter = 0
+    offset = 0
+    chat_object = InputChannel(chat['chat_id'], chat['access_hash'])
+    all_participants = []
+    print('Process...')
+    while True:
+        participants = client.invoke(GetParticipantsRequest(
                     chat_object, ChannelParticipantsSearch(''), offset, limit
                 ))
-		if not participants.users:
-			break
-		all_participants.extend(['{} {}'.format(x.id, x.username)
+        if not participants.users:
+            break
+        all_participants.extend(['{} {}'.format(x.id, x.username)
                            for x in participants.users])
-		users_count = len(participants.users)
-		offset += users_count
-		counter += users_count
-		print('{} users collected'.format(counter))
-		sleep(2)
-	with open('users.txt', 'w') as file:
-		file.write('\n'.join(map(str, all_participants)))
+        users_count = len(participants.users)
+        offset += users_count
+        counter += users_count
+        print('{} users collected'.format(counter))
+        sleep(2)
+    with open('users.txt', 'w') as file:
+        file.write('\n'.join(map(str, all_participants)))
 
 
 def main():
-	channel_name = input('Input a channel name, without "@": ')
-	client = TelegramClient('current-session', api_id, api_hash)
-	print('Connecting...')
-	client.connect()
-	if not client.is_user_authorized():
-		try:
-			client.send_code_request(phone)
-			print('Sending a code...')
-			client.sign_in(phone, code=input('Enter code: '))
-			print('Successfully!')
-		except FloodWaitError as FloodError:
-			print('Flood wait: {}.'.format(FloodError))
-			sys.exit()
-		except SessionPasswordNeededError:
-			client.sign_in(password=getpass('Enter password: '))
-			print('Successfully!')
-	dump_users(get_chat_info(channel_name, client), client)
-	print('Done!')
+    channel_name = input('Input a channel name, without "@": ')
+    client = TelegramClient('current-session', api_id, api_hash)
+    print('Connecting...')
+    client.connect()
+    if not client.is_user_authorized():
+        try:
+            client.send_code_request(phone)
+            print('Sending a code...')
+            client.sign_in(phone, code=input('Enter code: '))
+            print('Successfully!')
+        except FloodWaitError as FloodError:
+            print('Flood wait: {}.'.format(FloodError))
+            sys.exit()
+        except SessionPasswordNeededError:
+            client.sign_in(password=getpass('Enter password: '))
+            print('Successfully!')
+    dump_users(get_chat_info(channel_name, client), client)
+    print('Done!')
 
 if __name__ == '__main__':
-	main()
+    main()
